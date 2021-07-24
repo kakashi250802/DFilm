979e6fe4377d617d620cb21bfa975d5a16480a97
kakashi250802 <ngoduong37na@gmail.com>
2021-07-25 01:03:26 +0700

    fix some err

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,48 @@
+function renderData(todo_list) {
+    // console.log(todo_list.results)
+    var results = todo_list.results;
+    var htmls = results.map(function(todo_item) {
+        return `
+    <!-- slide item -->
+    <div class="slide">
+        <div class="info">
+            <div class="info-content">
+                <h2 class="top-down">
+                    ${todo_item.original_title}
+                </h2>
+                <h3 class="top-down trans-delay-0-2">
+                    ${todo_item.title}
+                </h3>
+                <p class="top-down trans-delay-0-4">
+                    ${todo_item.overview}
+                </p>
+                <div class="top-down trans-delay-0-6">
+                    <a href="#" class="btn-flat btn-hover">
+                        <span>Xem ngay <i class='bx bx-play-circle'></i></span>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="img">
+            <div style=" 
+            background:url('https://image.tmdb.org/t/p/w1280/${todo_item.backdrop_path}');                            
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+            width: 100vw;
+            height: 110vh;">
+            </div>
+        </div>
+    </div>
+    <!-- end slide item -->
+    `;
+    })
+
+
+    var html = htmls.join('');
+    document.querySelector('.slider > .container').insertAdjacentHTML('beforeend', html);
+}
+start();
 console.log(document.querySelectorAll('.slide'))
 let slide_index = 0;
 let slide_play = true;
